13efccbf28a6964133820c70ad1d7a216f1d5fcc
Vinicius <vini383@gmail.com>
2023-08-22 20:48:04 -0300

    feat: actions added

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,10 +1,14 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { increment } from './counter.actions';
 
 const initialState: number = 0;
 
-export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1)
+);
 
 //createReducer is not supported in older ngrx versions, alternative way to create:
-export function counterReducer1(state = initialState) {
-  return state;
-}
+// export function counterReducer1(state = initialState) {
+//   return state;
+// }
